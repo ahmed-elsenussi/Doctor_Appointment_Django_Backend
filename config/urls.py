6a25a90d7e16b66a7db1630e82db2c5abff5d052
ahmed <amhmdslah104@gmail.com>
2025-06-07 16:18:59 +0300

    fix requiremnts.txt 🐉 - renaming project to config 🛠🛠️

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    # includings
+    # other apps urls 
     path('users/', include('users.urls') ),
     path('doctors/', include('doctors.urls')), # for doctors and the specializations
     path('patients/', include('patients.urls')),
