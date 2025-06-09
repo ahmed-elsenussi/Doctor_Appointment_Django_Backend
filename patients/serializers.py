52bd12e2cfcdc18fdebc205cf57ee2daae924298
ahmed-kamal91 <nagmstar91@gmail.com>
2025-06-09 21:09:38 +0300

    mirror name from user table

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,8 +1,17 @@
-# wrtten by [SENU]
+# written by [SENU]
 from rest_framework import serializers
 from .models import Patient
 
 class PatientSerializer(serializers.ModelSerializer):
+
+    # [SENU]: MIRROR NAME FROM USER TABLE
+    name = serializers.SerializerMethodField()
+
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = ['patient_id', 'name', 'patient_image_path', 'phone', 'date_of_birth']
+
+    # IMPORTANT
+    def get_name(self, obj):
+        return obj.patient_id.name
+
