a2994d8e9bef5ba0c7af92867fe2c63b719318f5
ahmed-kamal91 <nagmstar91@gmail.com>
2025-06-09 21:05:54 +0300

    fetch patient data with fetching the appointments

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import Appointment
+from patients.models import Patient
+from patients.serializers import PatientSerializer
 
 # [SENU]: to validate and make the communication between the client and the backend
 class AppointmentSerializer(serializers.ModelSerializer):
+
+    # to also fetch the patient data [OLD IS GOLD]
+    # patient_id = PatientSerializer(read_only=True)
+
+    # PID , PATIENT
+    patient_id = serializers.PrimaryKeyRelatedField(queryset=Patient.objects.all())
+    patient = PatientSerializer(source='patient_id', read_only=True)
+
 
     class Meta:
         model = Appointment
